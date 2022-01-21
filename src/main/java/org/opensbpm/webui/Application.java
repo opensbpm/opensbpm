6547564a24dde99420581ad8b3b35899423a0bd3
Stefan Sedelmaier <stefan@sedelmaier.at>
2022-01-21 15:16:02 +0100

    fix imports

--- src/main/java/org/opensbpm/webui/Application.java
+++ src/main/java/org/opensbpm/webui/Application.java
@@ -1,23 +1,26 @@
 package org.opensbpm.webui;
 
-import at.softwaremacherei.elasticsearch.ElasticsearchConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
-import at.softwaremacherei.jsbpm.elasticsearch.ElasticSearchConfig;
 import org.opensbpm.engine.core.EngineConfig;
-import at.softwaremacherei.jsbpm.jasperreports.JasperReportsConfig;
+import org.opensbpm.plugins.jasperreports.JasperReportsConfig;
 import at.softwaremacherei.jsbpm.server.rbac.RbacConfig;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-@Import({EngineConfig.class,/*CrmJpaConfig.class,*/ RbacConfig.class,
-    JasperReportsConfig.class, ElasticSearchConfig.class, ElasticsearchConfig.class /*, DynamodbConfig.class*/})
+@Import({EngineConfig.class,
+    /*CrmJpaConfig.class,*/
+    RbacConfig.class,
+    JasperReportsConfig.class
+    /*,ElasticsearchConfig.class*/
+    /*, DynamodbConfig.class*/
+})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
