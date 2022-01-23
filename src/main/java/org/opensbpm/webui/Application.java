7ded10fedb3420e8c0381a98e9c02158b9ea0f06
Stefan Sedelmaier <stefan@sedelmaier.at>
2022-01-23 09:26:29 +0100

    swm rbac removed

--- src/main/java/org/opensbpm/webui/Application.java
+++ src/main/java/org/opensbpm/webui/Application.java
@@ -8,23 +8,27 @@ import org.springframework.context.annotation.Import;
 
 import org.opensbpm.engine.core.EngineConfig;
 import org.opensbpm.plugins.jasperreports.JasperReportsConfig;
-import at.softwaremacherei.jsbpm.server.rbac.RbacConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 @Import({EngineConfig.class,
-    /*CrmJpaConfig.class,*/
-    RbacConfig.class,
     JasperReportsConfig.class
-    /*,ElasticsearchConfig.class*/
-    /*, DynamodbConfig.class*/
 })
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+        //return NoOpPasswordEncoder.getInstance();
     }
 
 }
