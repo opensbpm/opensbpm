9f7378c097f41ec293beb0db209d7b6307f7849e
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-03-08 06:56:21 +0100

    Add test for fetching gerreting api

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,10 +2,31 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
+
 describe('App.test', () => {
     test('renders learn react link', () => {
         render(<App/>);
         const linkElement = screen.getByText(/learn react/i);
         expect(linkElement).toBeInTheDocument();
+    });
+
+    const greeting = {"id":1,"content":"Hello, World!"};
+    beforeEach(() => {
+        jest.spyOn(global, 'fetch').mockResolvedValue({
+            json: jest.fn().mockResolvedValue(greeting)
+        })
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+    test('renders greeting header',async () => {
+
+        render(<App/>);
+
+        const greetingNode = await screen.findByText("Hello, World!");
+
+        expect(greetingNode).toBeInTheDocument();
+
     });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState();
   useEffect(() => {
     const API_BASEURL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : '';
     fetch(`${API_BASEURL}/api/greeting`, { method: 'GET' })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => setGreeting(data.content))
         .catch((error) => console.log('ERROR in getGameCards: ' + error));
   }, []);
 
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+          <h1 data-testid="greeting">{greeting}</h1>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
 }
 
 export default App;
