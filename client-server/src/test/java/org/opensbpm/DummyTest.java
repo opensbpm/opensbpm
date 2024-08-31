14491564c045de2ff9521445bce156e7bc7392cf
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-08-31 14:48:48 +0200

    Add dummy test

---
+++ client-server/src/test/java/org/opensbpm/DummyTest.java
@@ -0,0 +1,11 @@
+package org.opensbpm;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void testDummy(){
+        System.out.println("testDummy");
+    }
+}
