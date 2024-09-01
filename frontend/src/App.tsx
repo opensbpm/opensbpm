aa0fb92b054f1f78e97ed1df19531eef3b066f97
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-09-01 20:31:35 +0200

    keycloak

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,78 +1,74 @@
-import React from 'react';
-import Keycloak from 'keycloak-js';
+import React, {useEffect, useRef, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import keycloak from './keycloak';
 
 function App() {
+    const wasCalled = useRef(false);
 
-  const keycloak = new Keycloak({
-    url: 'http://localhost:9000',
-    realm: 'quickstart',
-    clientId: 'authz-servlet'
-  });
+    useEffect(() => {
+        if(wasCalled.current) return;
+        wasCalled.current = true;
 
-  function login() {
-    try {
-      keycloak.init({onLoad: 'login-required'})
-          .then((authenticated) => {
-        if (authenticated) {
-          console.log('User is authenticated');
-        } else {
-          console.log('User is not authenticated');
-        }
-      });
-    } catch (error) {
-      console.error('Failed to initialize adapter:', error);
+        keycloak.init({
+            onLoad: 'login-required',
+            enableLogging: true
+        })
+            .then(authenticated => {
+                console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
+            })
+            .catch(error =>
+                console.error('Failed to initialize adapter:', error)
+            );
+    }, []);
+
+    function login() {
+        keycloak.login();
     }
 
-    //await keycloak.login();
-  }
+    function logout() {
+        keycloak.logout();
+    }
 
-  function showUserInfo() {
-    // keycloak.loadUserInfo()
-    //     .then(ui =>{
-    //       console.log(ui);
-    //     })
+    function showUserInfo() {
+        keycloak.loadUserProfile()
+            .then(result => console.log(result));
+    }
 
-    keycloak.loadUserProfile()
-        .then(ui =>{
-          console.log(ui);
+    function fetchResource() {
+        fetch("http://localhost:8090/",{
+          headers:{
+              authorization: `Bearer ${keycloak.token}`
+          },
         })
-  }
+            .then(res => {
+              console.log(res);
+            });
+    }
 
-  function fetchResource() {
-
-    fetch("http://localhost:8090/",{
-      headers:{
-        'Authorization': "Bearer "+keycloak.token
-      },
-    })
-        .then(res => {
-          console.log(res);
-        });
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={login}>Login</button>
-        <button onClick={showUserInfo}>UserInfo</button>
-        <button onClick={fetchResource}>Resource</button>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <div>{`User is ${!keycloak.authenticated ? 'not ' : ''}authenticated`}</div>
+                <button onClick={login}>Login</button>
+                <button onClick={logout}>Logout</button>
+                <button onClick={showUserInfo}>UserInfo</button>
+                <button onClick={fetchResource}>Resource</button>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
