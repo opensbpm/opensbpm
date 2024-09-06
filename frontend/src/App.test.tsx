fcaba257c15512e29b85cf56f4cb7641b33601bb
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-09-06 14:01:59 +0200

    Fix test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
+import {AuthProvider} from "react-oidc-context";
 
 describe('App.test', () => {
     test('renders learn react link', () => {
-        render(<App/>);
-        const linkElement = screen.getByText(/learn react/i);
+        render(
+            <AuthProvider>
+            <App/>
+            </AuthProvider>
+        );
+        const linkElement = screen.getByText(/loading/i);
         expect(linkElement).toBeInTheDocument();
     });
 });
