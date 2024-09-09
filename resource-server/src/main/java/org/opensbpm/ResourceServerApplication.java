3a4c3117c10872a148caf09fc6421620c73102ed
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-09-09 08:59:53 +0200

    Disable Certificate Validation

--- resource-server/src/main/java/org/opensbpm/ResourceServerApplication.java
+++ resource-server/src/main/java/org/opensbpm/ResourceServerApplication.java
@@ -3,10 +3,39 @@ package org.opensbpm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
 @SpringBootApplication
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (GeneralSecurityException e) {
+        }
         SpringApplication.run(ResourceServerApplication.class, args);
     }
 
