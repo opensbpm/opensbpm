33da24e0871e54c62a2366a3687aa168cdfae43b
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-09-20 14:36:45 +0200

    Move to dir vaadinui

--- vaadinui/src/main/java/org/opensbpm/webui/security/SecurityConfiguration.java
+++ vaadinui/src/main/java/org/opensbpm/webui/security/SecurityConfiguration.java
@@ -2,17 +2,10 @@ package org.opensbpm.webui.security;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.opensbpm.webui.ui.views.login.LoginView;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
