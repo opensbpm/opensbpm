82fdbd1498e9c2222619704487dba764e12f6e8a
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-09-21 11:46:16 +0200

    Rename webui to vaadinui

---
+++ vaadinui/src/main/java/org/opensbpm/vaadinui/Application.java
@@ -0,0 +1,29 @@
+package org.opensbpm.vaadinui;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@PWA(name = "OpenSBPM Vaadin Demo",
+        shortName = "OpenSBPM",
+        offlineResources = {
+                "./styles/offline.css",
+                "./images/offline.png"
+        }
+        /*enableInstallPrompt = false*/)
+@Theme("my-theme")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SslConfiguration.trustAll();
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ vaadinui/src/main/java/org/opensbpm/vaadinui/security/CustomRequestCache.java
@@ -0,0 +1,17 @@
+package org.opensbpm.vaadinui.security;
+
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+class CustomRequestCache extends HttpSessionRequestCache {
+
+    @Override
+    public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+        if (!SecurityUtils.isFrameworkInternalRequest(request)) {
+            super.saveRequest(request, response);
+        }
+    }
+
+}
