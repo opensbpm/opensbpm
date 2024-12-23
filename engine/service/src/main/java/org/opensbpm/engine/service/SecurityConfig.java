133133a74f538747a5adc79b694294f162176a5b
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-12-23 16:30:04 +0100

    Engine endpoints must be authenticated

--- engine/service/src/main/java/org/opensbpm/engine/service/SecurityConfig.java
+++ engine/service/src/main/java/org/opensbpm/engine/service/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
                         .requestMatchers("/services/openapi.yaml","/services/api-docs/**").permitAll()
-                        .requestMatchers("/services/**").permitAll()
+                        .requestMatchers("/services/engine/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt())
