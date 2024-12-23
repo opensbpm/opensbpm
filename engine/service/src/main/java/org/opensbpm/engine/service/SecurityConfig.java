d8414e4795a5298b0b5395c0155e94fe41a98e83
Sedelmaier Stefan <stefan@sedelmaier.at>
2024-12-23 15:59:15 +0100

    Allow swagger-ui to all

--- engine/service/src/main/java/org/opensbpm/engine/service/SecurityConfig.java
+++ engine/service/src/main/java/org/opensbpm/engine/service/SecurityConfig.java
@@ -18,9 +18,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/services/openapi.yaml","/services/api-docs/**").anonymous()
-                        //.requestMatchers(HttpMethod.GET, "/foos/**").hasAuthority("SCOPE_read")
-                        //.requestMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
+                        .requestMatchers("/services/openapi.yaml","/services/api-docs/**").permitAll()
+                        .requestMatchers("/services/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt())
