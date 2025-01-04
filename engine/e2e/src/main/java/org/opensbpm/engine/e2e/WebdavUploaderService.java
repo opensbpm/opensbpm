8e4ae5373aefcfb37641cf3cf6cd3b8d939a23c3
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-04 13:15:20 +0100

    Add WebdavUploader

---
+++ engine/e2e/src/main/java/org/opensbpm/engine/e2e/WebdavUploaderService.java
@@ -0,0 +1,32 @@
+package org.opensbpm.engine.e2e;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class WebdavUploaderService {
+
+    @Value("${e2e.statistics.url}")
+    private String url;
+
+    @Value("${e2e.statistics.username}")
+    private String username;
+
+    @Value("${e2e.statistics.password}")
+    private String password;
+
+    public void uploadStatistic(Configuration configuration, String statData) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        String fileName = String.format("statistics-%s-%s.csv", configuration.getProcessesCount(), LocalDateTime.now().format(formatter));
+
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .basicAuthentication(username, password)
+                .build();
+        restTemplate.put(String.format("%s/%s", url,fileName), statData);
+    }
+}
