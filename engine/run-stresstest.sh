b0510266b78b0ad55ed448778be3e7fff3b54183
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-15 08:34:32 +0100

    Rename stresstest-worker to stresstest

--- engine/run-stresstest.sh
+++ engine/run-stresstest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker run --rm --name stresstest-jdoe opensbpm/stresstest-worker --opensbpm.username=jdoe --opensbpm.password=jdoe &
-#docker run --rm --name stresstest-miriam opensbpm/stresstest-worker --opensbpm.username=miriam --opensbpm.password=miriam &
+#docker run --rm --name stresstest-jdoe opensbpm/stresstest --opensbpm.username=jdoe --opensbpm.password=jdoe &
+#docker run --rm --name stresstest-miriam opensbpm/stresstest --opensbpm.username=miriam --opensbpm.password=miriam &
 
 #mvn -pl engine/stresstestworker spring-boot:run -Dspring-boot.run.arguments="--opensbpm.username=jdoe --opensbpm.password=jdoe" &
 #mvn -pl engine/stresstestworker spring-boot:run -Dspring-boot.run.arguments="--opensbpm.username=miriam --opensbpm.password=miriam" &
@@ -21,7 +21,7 @@ for ((i=1; i<=steps; i++)); do
   # Run the Docker command the calculated number of times
   for ((j=1; j<=repetitions; j++)); do
     #echo "  Iteration $j for value $value"
-    docker run --rm --name stresstest-alice opensbpm/stresstest-worker \
+    docker run --rm --name stresstest-alice opensbpm/stresstest \
       --opensbpm.username=alice --opensbpm.password=alice \
       --opensbpm.starter=true \
       --opensbpm.statistics.nodes=1 \
