b4114e20c7df70b0309f45b4a77851e5af830d38
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-19 16:16:20 +0100

    Remove unused e2e Module

--- engine/e2e/src/main/java/org/opensbpm/engine/e2e/WorkflowExecution.java
+++
@@ -1,9 +0,0 @@
-package org.opensbpm.engine.e2e;
-
-import org.opensbpm.engine.client.userbot.Statistics;
-
-import java.util.List;
-
-public interface WorkflowExecution {
-    List<Statistics> execute();
-}
