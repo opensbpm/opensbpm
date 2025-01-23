f4aea795beda5643438a9bb38897b2bd41c88730
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-23 10:12:50 +0100

    Fix wrong comments

--- playbooks/roles/chaostest/files/chaostest.sh
+++ playbooks/roles/chaostest/files/chaostest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# show help and end programm
+# Show help and end programm
 show_help() { echo "Usage: $0 --interval <int> --processes <int> --config <name>"; exit 1; }
 
-# Argumente parsen
+# Parse arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --interval) INTERVAL="$2"; shift 2 ;;
@@ -13,16 +13,18 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# validate parameters
+# Validate parameters
 [[ "$INTERVAL" =~ ^[0-9]+$ && "$PROCESSES" =~ ^[0-9]+$ ]] || { echo "Error: Both --interval and --processes must be integers."; show_help; }
 [[ "$CONFIG" =~ ^[a-zA-Z0-9]+$ ]] || { echo "Error: --config must be alphanumeric."; show_help; }
 
 
+# Run userbot of users
 docker run --rm --detach --name userbot-jdoe opensbpm/userbot \
   --opensbpm.username=jdoe --opensbpm.password=jdoe
 docker run --rm --detach --name userbot-miriam opensbpm/userbot \
   --opensbpm.username=miriam --opensbpm.password=miriam
 
+# Run userbot of main user
 docker run --rm --name userbot-alice opensbpm/userbot \
   --opensbpm.username=alice --opensbpm.password=alice \
   --opensbpm.starter=true \
