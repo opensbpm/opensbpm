9e6cbe05cc453c209d4a8854c32d201a3cee53e2
Sedelmaier Stefan <stefan@sedelmaier.at>
2025-01-26 15:39:24 +0100

    Remove unused SslConfiguration.java

--- engine/service/src/main/java/org/opensbpm/engine/service/EngineServiceApplication.java
+++ engine/service/src/main/java/org/opensbpm/engine/service/EngineServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Import;
 public class EngineServiceApplication {
 
     public static void main(String[] args) {
-        SslConfiguration.trustAll();
         SpringApplication.run(EngineServiceApplication.class, args);
     }
 
